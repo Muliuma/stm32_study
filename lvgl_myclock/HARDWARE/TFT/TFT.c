22d15b2b639b376d2b0321b9c527875590902001
Muliuma <1848845107@qq.com>
2022-03-01 20:52:49 +0800

    no_1

---
+++ lvgl_myclock/HARDWARE/TFT/TFT.c
@@ -0,0 +1,274 @@
+#include "TFT.h" 
+#include "spi.h"
+#include "delay.h"	   
+#include "usart.h"	
+#include "led.h"
+
+
+//液晶IO初始化配置
+void LCD_GPIO_Init(void)
+{
+
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);//使能GPIOF时钟
+
+  //GPIOF9,F10初始化设置
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+  GPIO_Init(GPIOF, &GPIO_InitStructure);//初始化
+	
+	GPIO_SetBits(GPIOF,GPIO_Pin_9 | GPIO_Pin_10);//GPIOF9,F10设置高，灯灭
+	SPI1_Init();		   			//初始化SPI
+	LED_Init();	
+	SPI1_SetSpeed(SPI_BaudRatePrescaler_2);		//设置为21M时钟    
+}
+
+
+//向SPI总线传输一个8位数据
+void  SPI_WriteData(u8 Data)
+{
+   SPI1_ReadWriteByte(Data);
+}
+
+//向液晶屏写一个8位指令
+void Lcd_WriteIndex(u8 Index)
+{
+   //SPI 写命令时序开始
+   LED0=0;
+	 SPI_WriteData(Index);
+}
+
+//向液晶屏写一个8位数据
+void Lcd_WriteData(u8 Data)
+{
+   LED0=1;
+   SPI_WriteData(Data);
+}
+//向液晶屏写一个16位数据
+void LCD_WriteData_16Bit(u16 Data)
+{
+   LED0=1;
+	 SPI_WriteData(Data>>8); 	//写入高8位数据
+	 SPI_WriteData(Data); 			//写入低8位数据
+}
+
+void Lcd_WriteReg(u8 Index,u8 Data)
+{
+	Lcd_WriteIndex(Index);
+  Lcd_WriteData(Data);
+}
+
+void Lcd_Reset(void)
+{
+	LED1=0;
+	delay_ms(100);
+	LED1=1;
+	delay_ms(50);
+}
+
+//LCD Init For 1.44Inch LCD Panel with ST7735R.
+void Lcd_Init(void)
+{	
+	LCD_GPIO_Init();
+	Lcd_Reset(); //Reset before LCD Init.
+
+	//LCD Init For 1.44Inch LCD Panel with ST7735R.
+	Lcd_WriteIndex(0x11);//Sleep exit 
+	delay_ms (120);
+		
+	//ST7735R Frame Rate
+	Lcd_WriteIndex(0xB1); 
+	Lcd_WriteData(0x01); 
+	Lcd_WriteData(0x2C); 
+	Lcd_WriteData(0x2D); 
+
+	Lcd_WriteIndex(0xB2); 
+	Lcd_WriteData(0x01); 
+	Lcd_WriteData(0x2C); 
+	Lcd_WriteData(0x2D); 
+
+	Lcd_WriteIndex(0xB3); 
+	Lcd_WriteData(0x01); 
+	Lcd_WriteData(0x2C); 
+	Lcd_WriteData(0x2D); 
+	Lcd_WriteData(0x01); 
+	Lcd_WriteData(0x2C); 
+	Lcd_WriteData(0x2D); 
+	
+	Lcd_WriteIndex(0xB4); //Column inversion 
+	Lcd_WriteData(0x07); 
+	
+	//ST7735R Power Sequence
+	Lcd_WriteIndex(0xC0); 
+	Lcd_WriteData(0xA2); 
+	Lcd_WriteData(0x02); 
+	Lcd_WriteData(0x84); 
+	Lcd_WriteIndex(0xC1); 
+	Lcd_WriteData(0xC5); 
+
+	Lcd_WriteIndex(0xC2); 
+	Lcd_WriteData(0x0A); 
+	Lcd_WriteData(0x00); 
+
+	Lcd_WriteIndex(0xC3); 
+	Lcd_WriteData(0x8A); 
+	Lcd_WriteData(0x2A); 
+	Lcd_WriteIndex(0xC4); 
+	Lcd_WriteData(0x8A); 
+	Lcd_WriteData(0xEE); 
+	
+	Lcd_WriteIndex(0xC5); //VCOM 
+	Lcd_WriteData(0x0E); 
+	
+	Lcd_WriteIndex(0x36); //MX, MY, RGB mode 
+	Lcd_WriteData(0xC8); 
+	
+	//ST7735R Gamma Sequence
+	Lcd_WriteIndex(0xe0); 
+	Lcd_WriteData(0x0f); 
+	Lcd_WriteData(0x1a); 
+	Lcd_WriteData(0x0f); 
+	Lcd_WriteData(0x18); 
+	Lcd_WriteData(0x2f); 
+	Lcd_WriteData(0x28); 
+	Lcd_WriteData(0x20); 
+	Lcd_WriteData(0x22); 
+	Lcd_WriteData(0x1f); 
+	Lcd_WriteData(0x1b); 
+	Lcd_WriteData(0x23); 
+	Lcd_WriteData(0x37); 
+	Lcd_WriteData(0x00); 	
+	Lcd_WriteData(0x07); 
+	Lcd_WriteData(0x02); 
+	Lcd_WriteData(0x10); 
+
+	Lcd_WriteIndex(0xe1); 
+	Lcd_WriteData(0x0f); 
+	Lcd_WriteData(0x1b); 
+	Lcd_WriteData(0x0f); 
+	Lcd_WriteData(0x17); 
+	Lcd_WriteData(0x33); 
+	Lcd_WriteData(0x2c); 
+	Lcd_WriteData(0x29); 
+	Lcd_WriteData(0x2e); 
+	Lcd_WriteData(0x30); 
+	Lcd_WriteData(0x30); 
+	Lcd_WriteData(0x39); 
+	Lcd_WriteData(0x3f); 
+	Lcd_WriteData(0x00); 
+	Lcd_WriteData(0x07); 
+	Lcd_WriteData(0x03); 
+	Lcd_WriteData(0x10);  
+	
+	Lcd_WriteIndex(0x2a);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(0x7f);
+
+	Lcd_WriteIndex(0x2b);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(0x9f);
+	
+	Lcd_WriteIndex(0xF0); //Enable test command  
+	Lcd_WriteData(0x01); 
+	Lcd_WriteIndex(0xF6); //Disable ram power save mode 
+	Lcd_WriteData(0x00); 
+	
+	Lcd_WriteIndex(0x3A); //65k mode 
+	Lcd_WriteData(0x05); 
+	
+	
+	Lcd_WriteIndex(0x29);//Display on	 
+}
+
+
+/*************************************************
+函数名：LCD_Set_Region
+功能：设置lcd显示区域，在此区域写点数据自动换行
+入口参数：xy起点和终点
+返回值：无
+*************************************************/
+void Lcd_SetRegion(u16 x_start,u16 y_start,u16 x_end,u16 y_end)
+{		
+	Lcd_WriteIndex(0x2a);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(x_start+2);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(x_end+2);
+
+	Lcd_WriteIndex(0x2b);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(y_start+3);
+	Lcd_WriteData(0x00);
+	Lcd_WriteData(y_end+3);
+	
+	Lcd_WriteIndex(0x2c);
+
+}
+
+/*************************************************
+函数名：LCD_Set_XY
+功能：设置lcd显示起始点
+入口参数：xy坐标
+返回值：无
+*************************************************/
+void Lcd_SetXY(u16 x,u16 y)
+{
+  	Lcd_SetRegion(x,y,x,y);
+}
+
+	
+/*************************************************
+函数名：LCD_DrawPoint
+功能：画一个点
+入口参数：无
+返回值：无
+*************************************************/
+void Gui_DrawPoint(u16 x,u16 y,u16 Data)
+{
+	Lcd_SetRegion(x,y,x+1,y+1);
+	LCD_WriteData_16Bit(Data);
+}    
+
+/*****************************************
+ 函数功能：读TFT某一点的颜色                          
+ 出口参数：color  点颜色值                                 
+******************************************/
+unsigned int Lcd_ReadPoint(u16 x,u16 y)
+{
+  unsigned int Data;
+  Lcd_SetXY(x,y);
+
+  //Lcd_ReadData();//丢掉无用字节
+  //Data=Lcd_ReadData();
+  Lcd_WriteData(Data);
+  return Data;
+}
+/*************************************************
+函数名：Lcd_Clear
+功能：全屏清屏函数
+入口参数：填充颜色COLOR
+返回值：无
+*************************************************/
+void Lcd_Clear(u16 Color)               
+{	
+   unsigned int i,m;
+   Lcd_SetRegion(0,0,X_MAX_PIXEL-1,Y_MAX_PIXEL-1);
+   Lcd_WriteIndex(0x2C);
+   for(i=0;i<X_MAX_PIXEL;i++)
+    for(m=0;m<Y_MAX_PIXEL;m++)
+    {	
+	  	LCD_WriteData_16Bit(Color);
+    }   
+}
+
+
+
+
---
+++ lvgl_myclock/OBJ/lv_port_indev.d
@@ -0,0 +1,1 @@
+..\obj\lv_port_indev.o: ..\GUI\lvgl\examples\porting\lv_port_indev.c
